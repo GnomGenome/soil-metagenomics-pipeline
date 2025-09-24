9ce01203db044bb4e5039cb94d0b3f0c31b1a5b5
GnomGenome <kolesnikovaam2003@gmail.com>
2025-09-24 17:03:40 +0300

    Add files via upload

---
+++ 03_dehost_bowtie2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -euo pipefail
+
+SAMPLE_DIR="$1"
+THREADS=${THREADS:-28}
+
+# Путь к базе bowtie2 (без расширений .bt2)
+HUMAN_REF="/home/k/Documents/GENOMES/t2t/t2t.fna"
+INDEX_BASE="/home/k/Documents/GENOMES/t2t/human_genome_index"
+
+# Папка для результатов
+mkdir -p "$SAMPLE_DIR/bowtie_output"
+
+# Активация окружения
+eval "$(mamba shell hook --shell bash)"
+mamba activate bowtie
+
+# Создаём индекс, если его нет
+if [ ! -f "${INDEX_BASE}.1.bt2" ]; then
+    echo "[INFO] Индексы Bowtie2 не найдены. Создаю..."
+    bowtie2-build "$HUMAN_REF" "$INDEX_BASE"
+else
+    echo "[INFO] Использую существующие индексы Bowtie2."
+fi
+
+# Входные файлы (после тримминга)
+R1="$SAMPLE_DIR/paired_R1.fq.gz"
+R2="$SAMPLE_DIR/paired_R2.fq.gz"
+
+# SAM выход
+SAM_OUT="$SAMPLE_DIR/bowtie_output/${SAMPLE_DIR##*/}_to_human.sam"
+
+# Запуск bowtie2
+echo "[INFO] Запускаю bowtie2 для $(basename "$SAMPLE_DIR")..."
+bowtie2 -p $THREADS -x $INDEX_BASE \
+  -1 $R1 \
+  -2 $R2 \
+  --un-conc-gz \
+  $SAMPLE_DIR/non_human_clean \
+  > $SAM_OUT
+
+mv $SAMPLE_DIR/non_human_clean.1 $SAMPLE_DIR/non_human_clean_R1.fq.gz
+mv $SAMPLE_DIR/non_human_clean.2 $SAMPLE_DIR/non_human_clean_R2.fq.gz
+echo "[DONE] Де-хостинг завершён для $(basename "$SAMPLE_DIR")."
