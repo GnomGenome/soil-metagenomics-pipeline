3d820f761cbd0aedfca9a8183efa36e0a7ec4a3a
GnomGenome <kolesnikovaam2003@gmail.com>
2025-09-24 17:06:54 +0300

    Delete 01_fastqc.sh

--- 01_fastqc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Путь к папке с файлами
-SAMPLE_DIR="$1"
-
-# Запуск FastQC для всех .fq.gz файлов в папке
-mkdir -p "$SAMPLE_DIR/fastqc_output"
-fastqc "$SAMPLE_DIR"/*.fq.gz -o "$SAMPLE_DIR/fastqc_output"
-
